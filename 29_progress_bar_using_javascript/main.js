cbc4dacf95795d1ab840e6846d2e7eadec427e6d
sadiposeidon <sadiposeidon@gmail.com>
2023-06-29 00:03:11 +0400

    -

---
+++ 29_progress_bar_using_javascript/main.js
@@ -0,0 +1,16 @@
+const heading_element = window.document.querySelector("#heading");
+
+const content = "Lorem Ipsum is simply dummy text of the printing and typesetting industry 😍👏";
+
+let i = 0;
+
+function writeText() {
+    if (i > content.length) {
+        i = 0;
+    } else {
+        heading_element.innerHTML = content.slice(0, i);
+        i++;
+    }
+}
+
+setInterval(writeText, 80);
